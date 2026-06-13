c515e6f42711115a7d8e26608d88f7be512ce98c
Saurabh Desai <saurabhdesai19121@gmail.com>
2026-06-14 01:48:18 +0530

    Add:Models for Store.

---
+++ Store/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+
+# Create your models here.
+class Promotions(models.Model) :
+    description = models.TextField(max_length=255)
+    discount = models.FloatField()
+
+
+class Collection(models.Model) :
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
+
+
+class Product(models.Model) :
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotions)
+
+
+class Customer(models.Model) :
+    BRONZE_MEMBERSHIP = "B"
+    GOLD_MEMBERSHIP = "G"
+    PLATINUM_MEMBERSHIP = "P"
+
+    MEMBERSHIP_CHOICES = [
+        (BRONZE_MEMBERSHIP, "Bronze"),
+        (GOLD_MEMBERSHIP, "Gold"),
+        (PLATINUM_MEMBERSHIP, "Platinum"),
+    ]
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=10)
+    birthdate = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=BRONZE_MEMBERSHIP)
+
+class Address(models.Model) :
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class Orders(models.Model) :
+    PAYMENT_PENDING = "P"
+    PAYMENT_COMPLETE = "C"
+    PAYMENT_FAILED = "F"
+
+    PAYMNET_STATUS = [
+        (PAYMENT_PENDING, 'Pending'),
+        (PAYMENT_COMPLETE, 'Complete'),
+        (PAYMENT_FAILED, 'Failed')
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMNET_STATUS, default=PAYMENT_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+class OrderItem(models.Model) :
+    order=models.ForeignKey(Orders, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    qunatity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Cart(models.Model) :
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model) :
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
--- StoreFront/urls.py
+++ StoreFront/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from debug_toolbar.toolbar import debug_toolbar_urls
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('playground/', include("playground.urls")),
+] + debug_toolbar_urls()
