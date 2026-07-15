27a4fed1c3f7d1a288ea3d257be0fb2526078412
Saurabh Desai <saurabhdesai19121@gmail.com>
2026-07-16 05:05:04 +0530

    ADD:Searching

--- Store/views.py
+++ Store/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import status
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 from django.db.models import Count
 
@@ -13,8 +14,9 @@ class ProductViewSet(ModelViewSet) :
     queryset = Product.objects.all()
     serializer_class = serializers.ProductSerializer
 
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_class = ProductFilter
+    search_fields = ['title', 'description']
 
     def get_serializer_context(self):
         return {'request' : self.request}
